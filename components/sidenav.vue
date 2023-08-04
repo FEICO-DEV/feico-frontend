f637af81c7e0737421356ecbdb58bd1f8ce2db77
Supakorn Kimhajan <supakorn@Supakorns-MacBook-Pro.local>
2023-08-04 13:14:07 +0700

    side nav

--- components/sidenav.vue
+++ components/sidenav.vue
@@ -4,15 +4,15 @@
       <div class="top">
         <img src="/logo-white.png" class="img-fluid" />
         <nav class="mt-lg-5 mt-4">
-          <nuxt-link :to="localePath('about')"><h2 class="text-white">{{ $t('nav.aboutus') }}</h2></nuxt-link>
-          <nuxt-link :to="localePath('Product')"><h2 class="text-white">{{ $t('nav.product') }}</h2></nuxt-link>
-          <nuxt-link :to="localePath('Service')"><h2 class="text-white">{{ $t('nav.service') }}</h2></nuxt-link>
-          <h2 class="text-white">SOLAR BUSINESS ENABLER</h2>
-          <nuxt-link :to="localePath('News')"><h2 class="text-white">{{ $t('nav.press') }}</h2></nuxt-link>
-          <nuxt-link :to="localePath('success')"><h2 class="text-white">SUCCESS STORY</h2></nuxt-link>
-          <nuxt-link :to="localePath('contact')"><h2 class="text-white">CONTACT US</h2></nuxt-link>
-          <nuxt-link :to="localePath('career')"><h2 class="text-white">CAREER</h2></nuxt-link>
-          <h2 class="text-white">FREE SITE SURVEY</h2>
+          <nuxt-link :to="localePath('about')"><h4 class="text-white">{{ $t('nav.aboutus') }}</h4></nuxt-link>
+          <nuxt-link :to="localePath('Product')"><h4 class="text-white">{{ $t('nav.product') }}</h4></nuxt-link>
+          <nuxt-link :to="localePath('Service')"><h4 class="text-white">{{ $t('nav.service') }}</h4></nuxt-link>
+          <h4 class="text-white">SOLAR BUSINESS ENABLER</h4>
+          <nuxt-link :to="localePath('News')"><h4 class="text-white">{{ $t('nav.press') }}</h4></nuxt-link>
+          <nuxt-link :to="localePath('success')"><h4 class="text-white">SUCCESS STORY</h4></nuxt-link>
+          <nuxt-link :to="localePath('contact')"><h4 class="text-white">CONTACT US</h4></nuxt-link>
+          <nuxt-link :to="localePath('career')"><h4 class="text-white">CAREER</h4></nuxt-link>
+          <h4 class="text-white">FREE SITE SURVEY</h4>
         </nav>
       </div>
     </div>
